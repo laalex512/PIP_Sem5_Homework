392e6ec08d1df97ffc15f47226ff0d684b88119b
laalex <laalex512@gmail.com>
2022-09-19 19:58:03 +0300

    Add Task 2

--- tasks.py
+++ tasks.py
@@ -9,8 +9,87 @@
 # print(resultList)
 
 
-# import tkinter as tk
+# Создайте программу для игры с конфетами человек против человека.
+# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга.
+# Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет.
+# Все конфеты оппонента достаются сделавшему последний ход.
+# Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
+# a) Добавьте игру против бота
+# b) Подумайте как наделить бота ""интеллектом""
 
+# Чтобы победить первому игроку надо в первый ход взять столько конфет, чтобы осталось ближайшее число,
+# делящееся нацело на 29. Каждый следующий ход нужно делать чтобы за итерацию убывало 29 конфет.
+# Если второй игрок берет 14 (1, 28) конфет, то нужно брать 29-14 (1, 28) = 15 (28, 1) конфет.
+# В конце останется 29, и первый игрок победит в любом случае
+
+
+# from random import randint
+
+# countCandyes = 105
+# maxTake = 28
+
+# # 2 игрока:
+# whoTurns = False
+
+# while countCandyes > 0:
+#     currentTake = int(input(f"Player {int(whoTurns)+1} turn: "))
+#     if (0 < currentTake < 29) and (currentTake <= countCandyes):
+#         countCandyes -= currentTake
+#         print(f"{countCandyes} candyes left")
+#         if countCandyes == 0:
+#             print(f"Player {int(whoTurns)+1} wins")
+#         whoTurns = not whoTurns
+#     else:
+#         print("Incorrect count, try again")
+
+
+# # игрок против бота:
+
+# while countCandyes > 0:
+#     currentTake = int(input("Insert count of candyes you take: "))
+#     if (0 < currentTake < 29) and (currentTake <= countCandyes):
+#         countCandyes -= currentTake
+#         print(f"{countCandyes} candyes left")
+#         if countCandyes == 0:
+#             print("You win!")
+#             break
+#         if countCandyes <= maxTake:
+#             currentTake = randint(1, countCandyes)
+#         else:
+#             currentTake = randint(1, maxTake)
+#         countCandyes -= currentTake
+#         print(f"Bot take {currentTake} candyes")
+#         print(f"{countCandyes} candyes left")
+#         if countCandyes == 0:
+#             print("Bot win!")
+#     else:
+#         print("Incorrect count, try again")
+
+
+# # Бот ходит первым и выигрывает:
+
+# print(f"{countCandyes} candyes left")
+# while countCandyes > 0:
+#     currentTake = countCandyes % (maxTake+1)
+#     countCandyes -= currentTake
+#     print(f"Bot take {currentTake} candyes")
+#     print(f"{countCandyes} candyes left")
+#     if countCandyes == 0:
+#         print("Bot win!")
+#         break
+#     currentTake = int(input("Insert count of candyes you take: "))
+#     if (0 < currentTake < 29) and (currentTake <= countCandyes):
+#         countCandyes -= currentTake
+#         print(f"{countCandyes} candyes left")
+#         if countCandyes == 0:
+#             print("You win!")
+#     else:
+#         print("Incorrect count, try again")
+
+
+# Создайте программу для игры в ""Крестики-нолики"".
+
+# import tkinter as tk
 
 # window = tk.Tk()
 # label1 = tk.Label(window, text=f"Turn of X", font="times 15")
