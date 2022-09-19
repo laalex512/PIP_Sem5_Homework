7b24d07d2d75d63d3bf01cb48d4bd7fc5d3122ef
laalex <laalex512@gmail.com>
2022-09-19 19:41:45 +0300

    Add Task 1

---
+++ tasks.py
@@ -0,0 +1,9 @@
+# # Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+
+
+# initText = "абвпыд длодлф жоыж шопжывь выжабв абв выпщоф"
+# initList = initText.split()
+# findText = "абв"
+
+# resultList = list(filter(lambda x: findText not in x, initList))
+# print(resultList)
